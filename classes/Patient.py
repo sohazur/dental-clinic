dc122ae690f0c93983d0cc2e1b379bbbb3485827
Sohazur <mdsohazurislam@gmail.com>
2023-04-15 18:06:20 +0400

    Implemented Patient class

---
+++ classes/Patient.py
@@ -0,0 +1,9 @@
+import Person
+
+class Patient(Person):
+    def __init__(self, first_name, last_name, date_of_birth, phone_number, patient_id):
+        super().__init__(first_name, last_name, date_of_birth, phone_number)
+        self.__patient_id = patient_id
+
+    def get_patient_id(self):
+        return self.__patient_id
